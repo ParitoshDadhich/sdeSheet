ccca964df04466ff640927d3ff4ae38d7c2e3cf0
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-11-19 02:05:56 +0530

    Update missingAndRepeatingNumber.java

--- Arrays/Day1/missingAndRepeatingNumber.java
+++ Arrays/Day1/missingAndRepeatingNumber.java
@@ -1,3 +1,72 @@
+// method 1
+// time o(n) and space o(n)
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i: arr)
+            map.put(i, map.getOrDefault(i, 0) + 1);
+            
+
+        int mis = -1, rep = -1;
+        for(int i=1; i<=n; i++){
+            if(map.containsKey(i)){
+                if(map.get(i) > 1)
+                    rep = i;
+            } else
+                mis = i;
+        }
+        
+        int ans[] = new int[2];
+        ans[0] = rep;
+        ans[1] = mis;
+        
+        return ans;
+    }
+}
+
+// method 2
+// time o(n) and space o(1)
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        int sum1 = 0, sum2 = 0;
+        
+        // sum of elements
+        for(int i=1; i<=n; i++)
+            sum1 += i;
+        for(int i: arr)
+            sum2 += i;
+            
+        int const1 = sum1 - sum2;
+        
+        // square of sum
+        sum1 = 0;
+        sum2 = 0;
+        for(int i=1; i<=n; i++)
+            sum1 += i*i;
+        for(int i: arr)
+            sum2 += i*i;
+ 
+        int const2 = sum1 - sum2;
+
+        const2 = const2/const1;
+        
+        int mis = (const1 + const2)/2;
+        int rep = const2 - mis;
+       
+        int ans[] = {rep, mis}; 
+        return ans;
+    }
+}
+
+
+// method 3
+// time o(n) and space o(1)
+// using bit magic
+
+
 class Solve {
     int[] findTwoElement(int arr[], int n) {
         int xor = 0;
@@ -48,3 +117,40 @@ class Solve {
     }
 }
  
+
+// method 4
+// using swap sort
+// time o(n) and space o(1)
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        int mis = -1;
+        int rep = -1;
+        
+        int i = 0; 
+        while(i<n){
+            if(arr[i] == arr[arr[i]-1])
+                i++;
+            else
+                swap(arr, i, arr[i]-1);
+        }
+        
+        for(i=0; i<n; i++){
+            if(arr[i] != i+1){
+                mis = i+1;
+                rep = arr[i];
+            }
+        }
+        
+        int ans[] = {rep, mis};
+        return ans;
+        
+    }
+    
+    private static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+}
