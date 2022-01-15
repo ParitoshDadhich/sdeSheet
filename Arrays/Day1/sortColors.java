063ed91e334f1d38d4a94829077ae72625a37924
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2022-01-16 03:27:50 +0530

    solved with different approaches

--- Arrays/Day1/sortColors.java
+++ Arrays/Day1/sortColors.java
@@ -1,3 +1,33 @@
+// method 1
+// In Two pass
+
+class Solution {
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        int count0 = 0, count1 = 0, count2 = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0) count0++;
+            if(arr[i] == 1) count1++;
+            if(arr[i] == 2) count2++;
+        }
+        
+        for(int i=0; i<n; i++){
+            if(i < count0)
+                arr[i] = 0;
+            
+            else if(i < count0 + count1)
+                arr[i] = 1;
+            
+            else
+                arr[i] = 2;
+        }
+        
+    }
+}
+
+// method 2
+// In One pass
+
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
